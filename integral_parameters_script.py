5246f56700f41e358691b2ae88cf64e2eafb2f79
malbiruk <2601074@gmail.com>
2022-12-11 15:24:45 +0300

    refactor

--- integral_parameters_script.py
+++ integral_parameters_script.py
@@ -1,121 +1,145 @@
 #!/usr/bin/python3
 
+'''
+obtain and plot system parameters such as density profiles, area per lipid,
+thickness, Scd and cholesterol tilt angle
+'''
 
-from pathlib import Path
+from pathlib import Path, PosixPath
 import argparse
 import os
 import sys
+from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import axes
 from scipy.interpolate import make_interp_spline
 import MDAnalysis as mda
 from MDAnalysis.selections.gromacs import SelectionWriter
 
-from MHP_stats_script import flatten, sparkles, duration
+from mhp_stats_script import flatten, sparkles, duration
 
 
-def get_tpr_props(path, syst):
-    gmxutils = '/nfs/belka2/soft/impulse/dev/inst/gmx_utils.py'
-    return os.popen(f'{gmxutils} {path}/{syst}/md/md.tpr').read()
-
-
-def opener(inp):
-    with open(inp, 'r') as f:
+def opener(inp: PosixPath) -> list[str]:
+    '''
+    open text file as list of lines
+    '''
+    with open(inp, 'r', encoding='utf-8') as f:
         lines = [i.strip() for i in f.read().strip().split('\n')]
     return lines
 
 
-def get_chl_tilt(path, syst, b, e, dt):
-    print(f'🗄️ system:\t{syst}\n⌚️ time:\t{b}-{e} ns, dt={dt} ps')
+@dataclass
+class System:
+    '''
+    stores system name and path
+    '''
+    path: PosixPath  # directory containing system directory
+    name: str
+    dir: str
+
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+        self.dir = str(path / name)
+
+    def __repr__(self):
+        return f'System({self.name})'
+
+    def __str__(self):
+        return self.name
+
+    def get_tpr_props(self) -> str:
+        '''
+        obtain properties of tpr of the system
+        '''
+        gmxutils = '/nfs/belka2/soft/impulse/dev/inst/gmx_utils.py'
+        return os.popen(f'{gmxutils} {self.dir}/md/md.tpr').read()
+
+    def get_n_chols(self) -> str:
+        '''
+        obtain number of cholesterol molecules
+        '''
+        return [i.strip() for i in
+                [i for i in self.get_tpr_props().split('\n')
+                 if i.startswith('CHOL')][0].split('|')][1]
+
+    def resnames_from_systname(self) -> list[str]:
+        '''
+        obtain residue names of system
+        '''
+        no_numbers = ''.join([i for i in self.name if not i.isdigit()])
+        return [i.upper() if not i == 'chol' else 'CHL' for i in no_numbers.split('_')]
+
+
+@dataclass
+class TrajectorySlice:
+    '''
+    stores beginnig (b), ending (e) timepoints (in ns)
+    with dt (in ps) between them of system trajectory
+    '''
+    system: System
+    b: int
+    e: int
+    dt: int
+
+    def __repr__(self):
+        return f'TrajectorySlice({self.system.name}, ' \
+            f'b={self.b}, e={self.e}, dt={self.dt})'
+
+
+def get_chl_tilt(trj: TrajectorySlice) -> None:
+    '''
+    obtain cholesterol tilt angles
+    '''
+
+    print(
+        f'🗄️ system:\t{trj.system}\n⌚️ time:\t{trj.b}-{trj.e} ns, dt={trj.dt} ps')
     print('obtaining 💁 system 🏙️ information...')
-    tpr_props = get_tpr_props(path, syst)
-    n_chol = [i.strip() for i in
-              [i for i in tpr_props.split('\n') if i.startswith('CHOL')][0].split('|')][1]
-    u = mda.Universe(f'{path}/{syst}/md/md.tpr',
-                     f'{path}/{syst}/md/pbcmol.xtc', refresh_offsets=True)
+    u = mda.Universe(f'{trj.system.dir}/md/md.tpr',
+                     f'{trj.system.dir}/md/pbcmol.xtc',
+                     refresh_offsets=True)
     chols = u.residues[u.residues.resnames == 'CHL'].atoms
-
-    # c3 = ' '.join(
-    #     list(map(str, chols.select_atoms('name C3').indices.tolist())))
-    # c17 = ' '.join(
-    #     list(map(str, chols.select_atoms('name C17').indices.tolist())))
-
+    n_chol = len(u.residues[u.residues.resnames == 'CHL'])
     c3 = chols.select_atoms('name C3')
     c17 = chols.select_atoms('name C17')
 
-    with SelectionWriter(f'{path}/{syst}/ch3_ch17.ndx') as write_ndx:
+    with SelectionWriter(f'{trj.system.dir}/ch3_ch17.ndx') as write_ndx:
         write_ndx.write(c3, name='C3')
         write_ndx.write(c17, name='C17')
-
-    # with open(f'{path}/notebooks/chol_tilt/ch.ndx', 'w') as f:
-    #     f.write(f'[C3]\n{c3}\n[C17]\n{c17}\n')
 
     print('calculating 👨‍💻 cholesterol 🫀 tilt 📐 ...')
 
     cmd = ['source `ls -t /usr/local/gromacs*/bin/GMXRC | head -n 1 `',
-           f'gmx bundle -s {path}/{syst}/md/md.tpr',
-           f'-f {path}/{syst}/md/pbcmol.xtc',
-           f'-na {n_chol} -z -n {path}/{syst}/ch3_ch17.ndx',
-           f'-b {b}000 -e {e}000 -dt {dt}',
-           f'-ot {path}/notebooks/chol_tilt/{syst}_{b}-{e}-{dt}_tilt.xvg',
+           f'gmx bundle -s {trj.system.dir}/md/md.tpr',
+           f'-f {trj.system.dir}/md/pbcmol.xtc',
+           f'-na {n_chol} -z -n {trj.system.dir}/ch3_ch17.ndx',
+           f'-b {trj.b*1000} -e {trj.e * 1000} -dt {trj.dt}',
+           f'-ot {str(trj.system.path)}/notebooks/chol_tilt/'
+           f'{trj.system}_{trj.b}-{trj.e}-{trj.dt}_tilt.xvg',
            '-xvg none']
 
     os.popen(' '.join(cmd)).read()
-    lines = opener(f'{path}/notebooks/chol_tilt/{syst}_{b}-{e}-{dt}_tilt.xvg')
-    arr = np.array(
-        list(map(float, flatten([i.split()[1:] for i in lines]))))
-    np.savetxt(f'{path}/notebooks/chol_tilt/{syst}_{b}-{e}-{dt}_tilt.txt', arr)
     print('done ✅\n')
 
 
-def resnames_from_systname(syst):
-    no_numbers = ''.join([i for i in syst if not i.isdigit()])
-    return [i.upper() if not i == 'chol' else 'CHL' for i in no_numbers.split('_')]
-
-
-def get_densities(path, syst, b, e, dt):
-    groups = ['chols', 'chols_o', 'acyl_chains', 'phosphates', 'water']
-    print(f'🗄️ system:\t{syst}\n⌚️ time:\t{b}-{e} ns, dt={dt} ps')
-    (path / syst / 'density_profiles').mkdir(exist_ok=True)
-    print('obtaining 💁 system 🏙️ information...')
-
-    if 'chol' in syst:
-        tpr_props = get_tpr_props(path, syst)
-        n_chol = [i.strip() for i in
-                  [i for i in tpr_props.split('\n')
-                  if i.startswith('CHOL')][0].split('|')][1]
-    else:
-        for i in ('chols', 'chols_o'):
-            if i in groups:
-                groups.remove(i)
-
-    if not np.all([Path(f'{path}/{syst}/density_profiles/{gr}.ndx').is_file() for gr in groups]):
-        print('creating index files...')
-
-        u = mda.Universe(f'{path}/{syst}/md/md.tpr',
-                         f'{path}/{syst}/md/pbcmol.xtc', refresh_offsets=True)
-
-        mask = np.logical_or.reduce(
-            ([u.residues.resnames == res for res in resnames_from_systname(syst)]))
-        lipids = u.residues[mask].atoms
-
-        chols = u.residues[u.residues.resnames == 'CHL']
-
-        acyl_chains = sum([lip.atoms.select_atoms(
-            'smarts [C;$(CCCCC)] or smarts C=C',
-            rdkit_kwargs={'max_iter': 1000}).atoms
-            for lip in (lipids - chols.atoms).residues])
-
-        water = u.residues[u.residues.resnames == 'SOL'].atoms
-        phosphates = (lipids - chols.atoms).select_atoms('smarts OP(O)(=O)O',
-                                                         rdkit_kwargs={'max_iter': 1000}).atoms
-        cholesterol_o = chols.atoms.select_atoms('element O').atoms
-        individual_chols = [i.atoms for i in chols]
-
-        # create index files:
-        def create_index_file(path, syst, ind, group, lipids):
-            with SelectionWriter(f'{path}/{syst}/density_profiles/{ind}.ndx') \
+def get_densities(trj: TrajectorySlice) -> None:
+    '''
+    obtain densities of groups
+    '''
+    def create_index_files(system: System) -> None:
+        '''
+        create index files for groups
+        '''
+        def write_index_file(system: System,
+                             ind: str,
+                             group: mda.core.groups.AtomGroup,
+                             lipids: mda.core.groups.AtomGroup) -> None:
+            '''
+            write index fie for current group
+            '''
+            with SelectionWriter(f'{system.dir}/density_profiles/{ind}.ndx') \
                     as write_ndx:
                 write_ndx.write(lipids, name='LIPIDS')
                 if ind == 'chols':
@@ -124,28 +148,50 @@ def get_densities(path, syst, b, e, dt):
                 else:
                     write_ndx.write(group, name=ind)
 
-        if 'chol' in syst:
-            create_index_file(path, syst, 'chols', individual_chols, lipids)
-            create_index_file(path, syst, 'chols_o', cholesterol_o, lipids)
-        create_index_file(path, syst, 'acyl_chains', acyl_chains, lipids)
-        create_index_file(path, syst, 'phosphates', phosphates, lipids)
-        create_index_file(path, syst, 'water', water, lipids)
+        u = mda.Universe(f'{system.dir}/md/md.tpr',
+                         f'{system.dir}/md/pbcmol.xtc', refresh_offsets=True)
 
-    else:
-        print('skipping index files creation...')
+        mask = np.logical_or.reduce(
+            ([u.residues.resnames == res for res in trj.system.resnames_from_systname()]))
+        lipids = u.residues[mask].atoms
 
-    def obt_dens(path, syst, b, e, dt, ind):
-        if (path / syst / 'density_profiles' /
-                f'{ind}_{b}-{e}-{dt}_dp.xvg').is_file():
-            print(f'{ind}_{b}-{e}-{dt}_dp.xvg exists, skipping...')
+        chols = u.residues[u.residues.resnames == 'CHL']
+
+        acyl_chains = sum((lip.atoms.select_atoms(
+            'smarts [C;$(CCCCC)] or smarts C=C',
+            rdkit_kwargs={'max_iter': 1000}).atoms
+            for lip in (lipids - chols.atoms).residues))
+
+        water = u.residues[u.residues.resnames == 'SOL'].atoms
+        phosphates = (lipids - chols.atoms).select_atoms('smarts OP(O)(=O)O',
+                                                         rdkit_kwargs={'max_iter': 1000}).atoms
+        cholesterol_o = chols.atoms.select_atoms('element O').atoms
+        individual_chols = [i.atoms for i in chols]
+
+        if 'chol' in trj.system.name:
+            write_index_file(system, 'chols', individual_chols, lipids)
+            write_index_file(system, 'chols_o', cholesterol_o, lipids)
+        write_index_file(system, 'acyl_chains', acyl_chains, lipids)
+        write_index_file(system, 'phosphates', phosphates, lipids)
+        write_index_file(system, 'water', water, lipids)
+
+    def obt_dens(trj: TrajectorySlice, ind: str) -> None:
+        '''
+        obtain density for current group
+        '''
+        if (Path(trj.system.dir) / 'density_profiles' /
+                f'{ind}_{trj.b}-{trj.e}-{trj.dt}_dp.xvg').is_file():
+            print(f'{ind}_{trj.b}-{trj.e}-{trj.dt}_dp.xvg exists, skipping...')
         else:
             print(f'obtaining density of {ind}...')
+            if 'chol' in trj.system.name:
+                n_chol = trj.system.get_n_chols()
             cmd = ['source `ls -t /usr/local/gromacs*/bin/GMXRC | head -n 1 ` &&',
-                   f'gmx density -s {path}/{syst}/md/md.tpr',
-                   f'-f {path}/{syst}/md/pbcmol.xtc',
-                   f'-n {path}/{syst}/density_profiles/{ind}.ndx',
-                   f'-b {b}000 -e {e}000 -dt {dt} -sl 100',
-                   f'-o {path}/{syst}/density_profiles/{ind}_{b}-{e}-{dt}_dp.xvg',
+                   f'gmx density -s {trj.system.dir}/md/md.tpr',
+                   f'-f {trj.system.dir}/md/pbcmol.xtc',
+                   f'-n {trj.system.dir}/density_profiles/{ind}.ndx',
+                   f'-b {trj.b*1000} -e {trj.e*1000} -dt {trj.dt} -sl 100',
+                   f'-o {trj.system.dir}/density_profiles/{ind}_{trj.b}-{trj.e}-{trj.dt}_dp.xvg',
                    '-xvg none -center -symm']
             if ind == 'chols':
                 numbers = ' '.join([str(i) for i in range(int(n_chol) + 1)])
@@ -155,20 +201,45 @@ def get_densities(path, syst, b, e, dt):
                 cmd.insert(1, 'echo 0 1 |')
             os.popen(' '.join(cmd)).read()
 
+    groups = ['chols', 'chols_o', 'acyl_chains', 'phosphates', 'water']
+    print(
+        f'🗄️ system:\t{trj.system.name}\n⌚️ time:\t{trj.b}-{trj.e} ns, dt={trj.dt} ps')
+    (Path(trj.system.dir) / 'density_profiles').mkdir(exist_ok=True)
+    print('obtaining 💁 system 🏙️ information...')
+
+    if not 'chol' in trj.system.name:
+        for i in ('chols', 'chols_o'):
+            if i in groups:
+                groups.remove(i)
+
+    if not np.all(
+            [Path(f'{trj.system.dir}/density_profiles/{gr}.ndx').is_file()
+             for gr in groups]):
+        print('creating index files...')
+        create_index_files(trj.system)
+
+    else:
+        print('skipping index files creation...')
+
     for gr in groups:
-        obt_dens(path, syst, b, e, dt, gr)
+        obt_dens(trj, gr)
 
     print('done ✅\n')
 
 
-def plot_density_profile(ax, path, syst, b, e, dt):
-    print('plotting dp for \n'
-          f'🗄️ system:\t{syst}\n⌚️ time:\t{b}-{e} ns, dt={dt} ps')
-    groups = ['chols', 'chols_o', 'acyl_chains', 'phosphates', 'water'] \
-        if 'chol' in syst else ['acyl_chains', 'phosphates', 'water']
+def plot_density_profile(ax: axes._subplots.Axes, trj: TrajectorySlice) -> None:
+    '''
+    plot density profile of system on single axes
+    '''
 
-    dfs = {gr: pd.read_csv(f'{path}/{syst}/density_profiles/{gr}_{b}-{e}-{dt}_dp.xvg',
-                           header=None, delim_whitespace=True) for gr in groups}
+    print('plotting dp for \n'
+          f'🗄️ system:\t{trj.system}\n⌚️ time:\t{trj.b}-{trj.e} ns, dt={trj.dt} ps')
+    groups = ['chols', 'chols_o', 'acyl_chains', 'phosphates', 'water'] \
+        if 'chol' in trj.system.name else ['acyl_chains', 'phosphates', 'water']
+
+    dfs = {gr: pd.read_csv(
+        f'{trj.system.dir}/density_profiles/{gr}_{trj.b}-{trj.e}-{trj.dt}_dp.xvg',
+        header=None, delim_whitespace=True) for gr in groups}
 
     # sum cholesterol profiles
     if 'chols' in groups:
@@ -177,50 +248,60 @@ def plot_density_profile(ax, path, syst, b, e, dt):
 
     for gr in groups[::-1]:
         x, y = dfs[gr][0], dfs[gr][1]
-        x_y_Spline = make_interp_spline(x, y)
+        x_y_spline = make_interp_spline(x, y)
         x_ = np.linspace(x.min(), x.max(), 500)
-        y_ = x_y_Spline(x_)
+        y_ = x_y_spline(x_)
         ax.plot(x_, y_, label=gr)
         ax.legend()
-        ax.set_title(f'{syst}')
+        ax.set_title(trj.system.name)
         ax.set_xlabel('Z, nm')
         ax.set_ylabel('Density, kg/m³')
 
 
 def calc_1d_com(x, m):
+    '''
+    calculate center of mass of 1D array
+    '''
     return np.sum(x * m) / np.sum(m)
 
 
-def calculate_thickness(path, syst, b, e, dt):
+def calculate_thickness(trj: TrajectorySlice) -> list[float]:
+    '''
+    calculates thickness in each step of trajectory using phosphates densities
+    '''
     thickness = []
-    for fr in range(b, e, int(dt / 1000)):
-        get_densities(path, syst, fr, fr, 0)
-        df = pd.read_csv(f'{path}/{syst}/density_profiles/phosphates_{fr}-{fr}-0_dp.xvg',
+    for fr in range(trj.b, trj.e, int(trj.dt / 1000)):
+        get_densities(TrajectorySlice(trj.system, fr, fr, 0))
+        df = pd.read_csv(f'{trj.system.dir}/density_profiles/phosphates_{fr}-{fr}-0_dp.xvg',
                          header=None, delim_whitespace=True)
         x, y = df[0], df[1]
-        x_y_Spline = make_interp_spline(x, y)
+        x_y_spline = make_interp_spline(x, y)
         x_ = np.linspace(x.min(), x.max(), 500)
         thickness.append(
-            calc_1d_com(x_[x_ > 0], x_y_Spline(x_[x_ > 0]))
-            - calc_1d_com(x_[x_ < 0], x_y_Spline(x_[x_ < 0])))
+            calc_1d_com(x_[x_ > 0], x_y_spline(x_[x_ > 0]))
+            - calc_1d_com(x_[x_ < 0], x_y_spline(x_[x_ < 0])))
     return thickness
 
 
-def calculate_area_per_lipid(path, syst, b, e, dt):
-    if not Path(f'{path}/{syst}/md/box.xvg').is_file():
+def calculate_area_per_lipid(trj: TrajectorySlice) -> list[float]:
+    '''
+    calculates area per lipid in each step of trajectory
+    '''
+    if not Path(f'{trj.system.dir}/md/box.xvg').is_file():
         cmd = ['source `ls -t /usr/local/gromacs*/bin/GMXRC | head -n 1 ` &&',
-               f'echo 0 | gmx traj -f {path}/{syst}/md/pbcmol.xtc',
-               f'-s {path}/{syst}/md/md.tpr -noz',
-               f'-ob {path}/{syst}/md/box.xvg -xvg none -dt {dt} -b {b}000 -e {e}000']
+               f'echo 0 | gmx traj -f {trj.system.dir}/md/pbcmol.xtc',
+               f'-s {trj.system.dir}/md/md.tpr -noz',
+               f'-ob {trj.system.dir}/md/box.xvg -xvg none '
+               f'-dt {trj.dt} -b {trj.b*1000} -e {trj.e*1000}']
 
         os.popen(' '.join(cmd)).read()
 
-    box = pd.read_csv(f'{path}/{syst}/md/box.xvg',
+    box = pd.read_csv(f'{trj.system.dir}/md/box.xvg',
                       header=None, delim_whitespace=True, usecols=[1, 2], names=['x', 'y'])
 
     box['total area'] = box['x'] * box['y']
     box['area per lipid'] = box['total area'].apply(
-        lambda x: x / 400 if '20x20' in syst else x / 100)
+        lambda x: x / 400 if '20x20' in trj.system.name else x / 100)
 
     return box['area per lipid'].tolist()
 
@@ -228,6 +309,11 @@ def calculate_area_per_lipid(path, syst, b, e, dt):
 @sparkles
 @duration
 def main():
+    '''
+    parse arguments and obtain and plot system parameters such as
+    density profiles, area per lipid,thickness, Scd and cholesterol tilt angle
+    '''
+
     parser = argparse.ArgumentParser(
         description='Script to obtain integral parameters')
     parser.add_argument('--obtain_densities',
@@ -262,12 +348,10 @@ def main():
     }
 
     systems = flatten([(i, i + '_chol10', i + '_chol30', i + '_chol50')
-                       for exp in experiments for i in experiments[exp]])
+                       for i in flatten(experiments.values())])
 
     systems.remove('dopc_dops50_chol50')
     systems.remove('dopc_dops50')
-
-    b, e, dt = args.b, args.e, args.dt
 
     if args.obtain_densities:
         print('obtain all densities')
@@ -277,20 +361,21 @@ def main():
         arperlips = []
 
     for syst in systems:
+        trj_slice = TrajectorySlice(System(path, syst), args.b, args.e, args.dt)
         if args.obtain_densities:
-            get_densities(path, syst, b, e, dt)
+            get_densities(trj_slice)
         if args.obtain_thickness:
-            th = calculate_thickness(path, syst, b, e, dt)
+            th = calculate_thickness(trj_slice)
             thicknesses.append((syst, np.mean(th), np.std(th)))
         if args.obtain_arperlip:
-            arpl = calculate_area_per_lipid(path, syst, b, e, dt)
+            arpl = calculate_area_per_lipid(trj_slice)
             arperlips.append((syst, np.mean(arpl), np.std(arpl)))
 
     if args.obtain_thickness:
         print('saving thickness...')
         thick_df = pd.DataFrame.from_records(
             thicknesses, columns=['system', 'mean', 'std'])
-        thick_df.to_csv(path / 'notebooks' / 'thickness' / 'new_thickness.csv')
+        thick_df.to_csv(path / 'notebooks' / 'thickness' / 'new_thickness.csv', index=False)
         print('done.')
 
     if args.obtain_arperlip:
@@ -298,7 +383,7 @@ def main():
         arperlip_df = pd.DataFrame.from_records(
             arperlips, columns=['system', 'mean', 'std'])
         arperlip_df.to_csv(path / 'notebooks' /
-                           'area_per_lipid' / 'new_arperlip.csv')
+                           'area_per_lipid' / 'new_arperlip.csv', index=False)
         print('done')
 
 
