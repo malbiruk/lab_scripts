f8813d8294fa6204937920508882335554309c41
malbiruk <2601074@gmail.com>
2022-12-12 21:29:40 +0300

    integral done

--- integral_parameters_script.py
+++ integral_parameters_script.py
@@ -6,12 +6,12 @@ thickness, Scd and cholesterol tilt angle
 '''
 
 from pathlib import Path, PosixPath
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 from typing import Callable
 import argparse
 import os
 import sys
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -56,6 +56,7 @@ class System:
     '''
     path: PosixPath  # directory containing system directory
     name: str
+    dir: str = field(init=False)
 
     def __post_init__(self):
         object.__setattr__(self, 'dir', str(self.path / self.name))
@@ -83,10 +84,16 @@ class System:
 
     def resnames_from_systname(self) -> list[str]:
         '''
-        obtain residue names of system
+        obtain residue names of system (MDAnalysis format)
         '''
         no_numbers = ''.join([i for i in self.name if not i.isdigit()])
         return [i.upper() if not i == 'chol' else 'CHL' for i in no_numbers.split('_')]
+
+    def pl_selector(self, n=0) -> str:
+        '''
+        obtain selector string of main phospholipid (impulse format)
+        '''
+        return f"'{self.name.split('_')[n].upper()}///'"
 
 
 @dataclass(frozen=True, unsafe_hash=True)
@@ -283,11 +290,15 @@ def calc_1d_com(x, m):
 
 def calculate_thickness(trj: TrajectorySlice) -> list[float]:
     '''
-    calculates thickness in each step of trajectory using phosphates densities
+    calculates thickness in each step of trajectory part using phosphates densities
     '''
     thickness = []
+    trj_list = [TrajectorySlice(trj.system, fr, fr, 0)
+                for fr in range(trj.b, trj.e, int(trj.dt / 1000))]
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        executor.map(get_densities, trj_list)
+
     for fr in range(trj.b, trj.e, int(trj.dt / 1000)):
-        get_densities(TrajectorySlice(trj.system, fr, fr, 0))
         df = pd.read_csv(f'{trj.system.dir}/density_profiles/phosphates_{fr}-{fr}-0_dp.xvg',
                          header=None, delim_whitespace=True)
         x, y = df[0], df[1]
@@ -322,8 +333,159 @@ def calculate_area_per_lipid(trj: TrajectorySlice) -> list[float]:
     return box['area per lipid'].tolist()
 
 
-@sparkles
-@duration
+def run_scd_py(trj: TrajectorySlice) -> None:
+    '''
+    calculates per atom Scd values for the first PL in system name for whole trajectory part
+    '''
+    if not Path(f'{trj.system.path}/notebooks/scd/'
+                f'{trj.system.name}_{trj.b}-{trj.e}-{trj.dt}_scd.csv').is_file():
+        cmd = ['/nfs/belka2/soft/impulse/dev/inst/scd.py',
+               f'-f {trj.system.dir}/md/pbcmol.xtc',
+               f'-s {trj.system.dir}/md/md.tpr',
+               f'-o {trj.system.path}/notebooks/scd/{trj.system.name}_{trj.b}-{trj.e}-{trj.dt}',
+               f'-b {trj.b*1000} -e {trj.e*1000} --dt {trj.dt} --sel {trj.system.pl_selector()}']
+        print(
+            f'calculating Scd for {trj.system.name}, {trj.b}-{trj.e} ns, dt={trj.dt} ps...')
+        os.popen(' '.join(cmd)).read()
+    else:
+        print(
+            f'Scd file for {trj.system.name}, '
+            f'{trj.b}-{trj.e} ns, dt={trj.dt} ps exists, skipping...')
+
+
+def calculate_scd(trj: TrajectorySlice) -> None:
+    '''
+    calculates per atom Scd values for the first PL in system name for each step in trajectory part
+    '''
+    trj_list = [TrajectorySlice(trj.system, fr, fr, 0)
+                for fr in range(trj.b, trj.e, int(trj.dt / 1000))]
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        executor.map(run_scd_py, trj_list)
+
+
+def scd_summary(trj_slices: list[TrajectorySlice]) -> None:
+    '''
+    averages data from individual _scd.csv files generated earlier and creates tables
+    scd_all.csv, scd_atoms.csv and scd_chains,csv
+    '''
+    scd_folder = Path('/home/klim/Documents/chol_impact/notebooks/scd')
+
+    systems = []
+    times = []
+    chains = []
+    atoms = []
+    scds = []
+
+    print('aggregating data...')
+
+    for trj in trj_slices:
+        scd_files = [str(scd_folder / f'{trj.system.name}_{i}-{i}-0_scd.csv')
+                     for i in range(trj.b, trj.e, int(trj.dt / 1000))]
+        for file in scd_files:
+            lines = opener(file)
+            systems.extend([trj.system.name for i in range(
+                len(lines[1].split(',')) + len(lines[5].split(',')))])
+            time = ' '.join([file.split('-')[1].split('_')[0], 'ns'])
+            times.extend([time for i in range(
+                len(lines[1].split(',')) + len(lines[5].split(',')))])
+            chains.extend(['sn-2' for i in range(len(lines[1].split(',')))])
+            chains.extend(['sn-1' for i in range(len(lines[5].split(',')))])
+            atoms.extend(lines[1].split(','))
+            atoms.extend(lines[5].split(','))
+            scds.extend(lines[3].split(','))
+            scds.extend(lines[7].split(','))
+
+    print('creating summary table...')
+    df = pd.DataFrame({'system': systems, 'timepoint': times,
+                      'chain': chains, 'atom': atoms, 'scd': scds})
+    df.to_csv(scd_folder / 'scd_all_new.csv', index=False)
+
+    print('summary table for atoms...')
+    df = pd.read_csv(scd_folder / 'scd_all_new.csv')
+    atom_summary = df.groupby(['system', 'atom'], as_index=False).agg(
+        'mean').rename({'scd': 'mean'}, axis=1)
+    atom_summary = atom_summary.assign(std=df.groupby(
+        ['system', 'atom']).agg(np.std)['scd'].values)
+    atom_summary.to_csv(scd_folder / 'scd_atoms.csv', index=False)
+
+    print('summary table for chains...')
+    df = pd.read_csv(scd_folder / 'scd_all_new.csv')
+    chain_summary = df.groupby(['system', 'chain'], as_index=False).agg(
+        'mean').rename({'scd': 'mean'}, axis=1)
+    chain_summary = chain_summary.assign(std=df.groupby(
+        ['system', 'chain']).agg(np.std)['scd'].values)
+    chain_summary.to_csv(scd_folder / 'scd_chains.csv', index=False)
+
+
+def integral_summary(infile: PosixPath,
+                     outfile: PosixPath,
+                     index=None) -> None:
+    '''
+    move chol rows to new columns and calculate relative_changes
+    '''
+    def move_chol_rows_to_new_columns(
+            infile: PosixPath,
+            outfile: PosixPath,
+            index=None) -> None:
+        '''
+        system       | val          system | chol0 | chol10 | chol30 | chol50
+        popc            1           popc       1        2        3        4
+        popc_chol10     2      ->
+        popc_chol30     3
+        popc_chol50     4
+
+        concat dataframes by 'index' argument
+        '''
+
+        if index is None:
+            index = ['system']
+
+        df = pd.read_csv(infile)
+        df.sort_values('system', inplace=True, ignore_index=True)
+
+        df_parts = []
+        for i in [10, 30, 50]:
+            df_part = df[df['system'].str.contains(
+                f'_chol{i}')].copy().reset_index(drop=True)
+            df_part['system'] = pd.Series(
+                ['_'.join(i.split('_')[:-1]) for i in df_part.loc[:, 'system']])
+            df_part.columns = ['system'] + \
+                [f'{name}_chol{i}' if not name in index else name for name in df_part.columns[1:]]
+            df_parts.append(df_part)
+
+        df.drop(df[df['system'].str.contains('chol')].index, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        df_concat = pd.concat([df.set_index(index)] +
+                              [i.set_index(index) for i in df_parts], axis=1)
+
+        df_concat.to_csv(outfile)
+
+    def calculate_relative_changes(infile: PosixPath, n_of_index_cols: int = 1) -> None:
+        '''
+        calculate relative changes for parameters and save them as new file in the same directory
+        '''
+        df = pd.read_csv(infile, index_col=list(range(n_of_index_cols)))
+        df2 = pd.DataFrame(index=df.index)
+        for i in [10, 30, 50]:
+            df2[f'chol{i} change (%)'] = (
+                df[f'mean_chol{i}'] - df['mean']) / df['mean'] * 100
+            df2[f'chol{i} change (%) std'] = np.sqrt(np.abs(
+                (df[f'std_chol{i}']**2 * df['mean']**2
+                 - df['std']**2 * df[f'mean_chol{i}']**2) / df['mean']**4)) * 100
+        df2.to_csv(str(infile).split('.')[0] + '_relative_changes.csv')
+    move_chol_rows_to_new_columns(infile, outfile, index)
+    calculate_relative_changes(
+        outfile, 1) if index is None else calculate_relative_changes(outfile, len(index))
+
+# TODO: plotting: dp, scd
+# TODO: plotting: integral
+# TODO: com - phosphates, o - phosphates, peak width
+# TODO: angles, angles + densities (horizontal component percentage)
+
+
+@ sparkles
+@ duration
 def main():
     '''
     parse arguments and obtain and plot system parameters such as
@@ -341,6 +503,13 @@ def main():
     parser.add_argument('--obtain_arperlip',
                         action='store_true',
                         help='obtain area per lipid data')
+    parser.add_argument('--obtain_scd',
+                        action='store_true',
+                        help='obtain scd data')
+    parser.add_argument('--integral_summary',
+                        action='store_true',
+                        help='generate summary tables as well as relative value changes '
+                        'for integral parameters')
     parser.add_argument('--dt', type=int, default=1000,
                         help='dt in ps')
     parser.add_argument('--b', type=int, default=150,
@@ -360,7 +529,7 @@ def main():
     experiments = {
         'chain length': ('dmpc', 'dppc_325', 'dspc'),
         'chain saturation': ('dppc_325', 'popc', 'dopc'),
-        'head polarity': ('dopc', 'dopc_dops50', 'dops'),
+        'head polarity': ('dopc', 'dopc_dops30', 'dopc_dops50', 'dops'),
     }
 
     systems = flatten([(i, i + '_chol10', i + '_chol30', i + '_chol50')
@@ -369,28 +538,18 @@ def main():
     systems.remove('dopc_dops50_chol50')
     systems.remove('dopc_dops50')
 
+    trj_slices = [TrajectorySlice(
+        System(path, s), args.b, args.e, args.dt) for s in systems]
+
     if args.obtain_densities:
         print('obtain all densities')
-    if args.obtain_thickness:
-        thicknesses = []
-    if args.obtain_arperlip:
-        arperlips = []
-
-    trj_slices = [TrajectorySlice(System(path, s), args.b, args.e, args.dt) for s in systems]
-
-    if args.obtain_densities:
         with ProcessPoolExecutor(max_workers=8) as executor:
             executor.map(get_densities, trj_slices)
 
     if args.obtain_thickness:
+        thicknesses = []
         for trj, th in multiproc(calculate_thickness, trj_slices, 8).items():
             thicknesses.append((trj.system.name, np.mean(th), np.std(th)))
-
-    if args.obtain_arperlip:
-        for trj, arpl in multiproc(calculate_area_per_lipid, trj_slices, 8).items():
-            arperlips.append((trj.system.name, np.mean(arpl), np.std(arpl)))
-
-    if args.obtain_thickness:
         print('saving thickness...')
         thick_df = pd.DataFrame.from_records(
             thicknesses, columns=['system', 'mean', 'std'])
@@ -399,11 +558,44 @@ def main():
         print('done.')
 
     if args.obtain_arperlip:
+        arperlips = []
+        for trj, arpl in multiproc(calculate_area_per_lipid, trj_slices, 8).items():
+            arperlips.append((trj.system.name, np.mean(arpl), np.std(arpl)))
         print('saving area per lipid...')
         arperlip_df = pd.DataFrame.from_records(
             arperlips, columns=['system', 'mean', 'std'])
         arperlip_df.to_csv(path / 'notebooks' /
                            'area_per_lipid' / 'new_arperlip.csv', index=False)
+        print('done.')
+
+    if args.obtain_scd:
+        print('obtain all scd')
+        with ProcessPoolExecutor(max_workers=8) as executor:
+            executor.map(calculate_scd, trj_slices)
+        print('saving scd...')
+        scd_summary(trj_slices)
+        print('done.')
+
+    if args.integral_summary:
+        print('reformatting integral parameters...')
+        integral_summary(
+            path / 'notebooks' / 'thickness' / 'new_thickness.csv',
+            path / 'notebooks' / 'integral_parameters' / 'thickness.csv'
+        )
+        integral_summary(
+            path / 'notebooks' / 'area_per_lipid' / 'new_arperlip.csv',
+            path / 'notebooks' / 'integral_parameters' / 'arperlip.csv'
+        )
+        integral_summary(
+            path / 'notebooks' / 'scd' / 'res' / 'scd_chains.csv',
+            path / 'notebooks' / 'integral_parameters' / 'scd_chains.csv',
+            ['system', 'chain']
+        )
+        integral_summary(
+            path / 'notebooks' / 'scd' / 'res' / 'scd_atoms.csv',
+            path / 'notebooks' / 'integral_parameters' / 'scd_atoms.csv',
+            ['system', 'atom']
+        )
         print('done.')
 
 
