b2342d22dab2849e78ed0cd8266c51717fec8774
malbiruk <2601074@gmail.com>
2022-12-16 12:56:27 +0300

    plot angles_density final

--- integral_parameters_script.py
+++ integral_parameters_script.py
@@ -386,7 +386,7 @@ def chl_tilt_summary(trj_slices: list[TrajectorySlice]) -> None:
             list(map(float, flatten([i.split()[1:] for i in lines])))) - 90
         n_chols = int(a.shape[0] / timepoint.shape[0])
         timepoints = [i for i in timepoint for _ in range(n_chols)]
-        chl_indices = flatten([[i for i in range(n_chols)] for _ in timepoint])
+        chl_indices = flatten([list(range(n_chols)) for _ in timepoint])
         records.append((trj.system.name, timepoints, chl_indices, a))
     df = pd.DataFrame.from_records(
         records, columns=['system', 'timepoint', 'chl_index', 'α, °'])
@@ -567,7 +567,7 @@ def plot_scd_atoms(experiments: dict, trj_slices: list[TrajectorySlice]) -> None
         axs[0].set_ylabel('Scd')
         handles, labels = axs[0].get_legend_handles_labels()
         fig.legend(handles, labels)
-        fig.suptitle(exp, fontsize=16)
+        fig.suptitle(exp)
         plt.savefig(trj_slices[0].system.path / 'notebooks' / 'integral_parameters' /
                     f'{"_".join(exp.split())}_'
                     f'scd_{trj_slices[0].b}_{trj_slices[0].e}_{trj_slices[0].dt}.png',
@@ -585,74 +585,77 @@ def plot_scd_atoms(experiments: dict, trj_slices: list[TrajectorySlice]) -> None
     scd_ms = scd_ms.reset_index(level=1).reset_index(
         level=1).reset_index(level=1).reset_index()
 
-    print('plotting dp by experiment...')
+    print('plotting scd by experiment...')
 
     for exp, systs in experiments.items():
         scd_plot(scd_ms, exp, systs)
     print('done.')
 
 
-def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice]) -> None:
+def obtain_df_of_density(trj_slices: list[TrajectorySlice]) -> pd.DataFrame:
     '''
-    calculate percentage of chl tilt horizontal component in systems
-    and plot it together with density plots;
-    9 plots per experiment
+    summarize data from different files of density to one df
     '''
-    def obtain_df_of_density(trj_slices: list[TrajectorySlice]) -> pd.DataFrame:
-        '''
-        summarize data from different files of density to one df
-        '''
-        records = []
-        for trj in trj_slices:
-            for fr in range(trj_slices[0].b, trj_slices[0].e, int(trj_slices[0].dt / 1000)):
-                lines = opener(Path(trj.system.dir) / 'density_profiles' /
-                               f'chols_{fr}-{fr}-0_dp.xvg')
-                z = np.array(
-                    list(map(float, [i.split()[0] for i in lines])))
-                a = np.array(
-                    list(map(float, flatten([i.split()[1:] for i in lines]))))
-                n_chols = int(a.shape[0] / z.shape[0])
-                z_col = [i for i in z for _ in range(n_chols)]
-                chl_indices = flatten([[i for i in range(n_chols)] for _ in z])
-                records.append(
-                    (trj.system.name, fr * 1000, chl_indices, z_col, a))
-        df = pd.DataFrame.from_records(
-            records, columns=['system', 'timepoint', 'chl_index', 'z', 'density'])
-        df.sort_values('system', inplace=True, ignore_index=True)
-        df['CHL amount, %'] = df['system'].str.split(
-            '_chol', n=1, expand=True)[1]
-        df['system'] = df['system'].str.split('_chol', n=1, expand=True)[0]
-        df.replace(to_replace=[None], value=0, inplace=True)
-        return df.explode(['chl_index', 'z', 'density'])
+    records = []
+    for trj in trj_slices:
+        for fr in range(trj_slices[0].b, trj_slices[0].e, int(trj_slices[0].dt / 1000)):
+            lines = opener(Path(trj.system.dir) / 'density_profiles' /
+                           f'chols_{fr}-{fr}-0_dp.xvg')
+            z = np.array(
+                list(map(float, [i.split()[0] for i in lines])))
+            a = np.array(
+                list(map(float, flatten([i.split()[1:] for i in lines]))))
+            n_chols = int(a.shape[0] / z.shape[0])
+            z_col = [i for i in z for _ in range(n_chols)]
+            chl_indices = flatten([list(range(n_chols)) for _ in z])
+            records.append(
+                (trj.system.name, fr * 1000, chl_indices, z_col, a))
+    df = pd.DataFrame.from_records(
+        records, columns=['system', 'timepoint', 'chl_index', 'z', 'density'])
+    df.sort_values('system', inplace=True, ignore_index=True)
+    df['CHL amount, %'] = df['system'].str.split(
+        '_chol', n=1, expand=True)[1]
+    df['system'] = df['system'].str.split('_chol', n=1, expand=True)[0]
+    df.replace(to_replace=[None], value=0, inplace=True)
+    return df.explode(['chl_index', 'z', 'density'])
 
-    def angle_refers_to_component(syst, angle, chl) -> tuple:
-        '''
-        depending on system and chl amount determine component:
-        1 vertical, 2 horizontal
-        returns tuple (1,), (2,) or (1,2)
 
-        ctr +- 1/2 wid => angle in component
-        components are located in notebooks/chol_tilt/{syst}_100-200-100_4_comps.csv
-        '''
-        comps = pd.read_csv('/home/klim/Documents/chol_impact/'
-                            f'notebooks/chol_tilt/{syst}_chol{chl}_100-200-100_4_comps.csv')
-        res = []
-        angle = -9
-        if (angle >= comps.loc[0, 'ctr'] - comps.loc[0, 'wid'] / 2
-            and angle <= comps.loc[0, 'ctr'] + comps.loc[0, 'wid'] / 2
-            or angle >= comps.loc[3, 'ctr'] - comps.loc[3, 'wid'] / 2
-                and angle <= comps.loc[3, 'ctr'] + comps.loc[3, 'wid'] / 2):
+def angle_refers_to_component(syst, angle, chl) -> tuple:
+    '''
+    depending on system and chl amount determine component:
+    1 vertical, 2 horizontal
+    returns tuple (1,), (2,) or (1,2)
+
+    ctr +- 1/2 wid => angle in component
+    components are located in notebooks/chol_tilt/{syst}_100-200-100_4_comps.csv
+    '''
+    comps = pd.read_csv('/home/klim/Documents/chol_impact/'
+                        f'notebooks/chol_tilt/{syst}_chol{chl}_100-200-100_4_comps.csv')
+    res = []
+    if angle >= 0:
+        if (comps.loc[3, 'ctr'] - comps.loc[3, 'wid'] / 2
+                <= angle <= comps.loc[3, 'ctr'] + comps.loc[3, 'wid'] / 2):
             res.append(2)
-        if (angle >= comps.loc[1, 'ctr'] - comps.loc[1, 'wid'] / 2
-            and angle <= comps.loc[1, 'ctr'] + comps.loc[1, 'wid'] / 2
-            or angle >= comps.loc[2, 'ctr'] - comps.loc[2, 'wid'] / 2
-                and angle <= comps.loc[2, 'ctr'] + comps.loc[2, 'wid'] / 2):
+        if (comps.loc[2, 'ctr'] - comps.loc[2, 'wid'] / 2
+                <= angle <= comps.loc[2, 'ctr'] + comps.loc[2, 'wid'] / 2):
             res.append(1)
-        return tuple(res)
+    if angle < 0:
+        if (comps.loc[0, 'ctr'] + comps.loc[0, 'wid'] / 2
+                >= angle >= comps.loc[0, 'ctr'] - comps.loc[0, 'wid'] / 2):
+            res.append(2)
+        if (comps.loc[1, 'ctr'] + comps.loc[1, 'wid'] / 2
+                >= angle >= comps.loc[1, 'ctr'] - comps.loc[1, 'wid'] / 2):
+            res.append(1)
+    return tuple(res)
 
-    trj_slices = [s for s in trj_slices if 'chol' in s.system.name]
+
+def create_tilt_density_table(trj_slices: list[TrajectorySlice]) -> None:
+    '''
+    calculate fractions of horizontal component depending on density and z coordinate
+    '''
+
     df = pd.read_csv(trj_slices[0].system.path / 'notebooks' /
-                     'integral_parameters' / f'chl_tilt_100-200-100.csv')
+                     'integral_parameters' / 'chl_tilt_100-200-100.csv')
     df = df[df['timepoint'].astype(str).str.fullmatch('|'.join(
         [str(i) for i in range(trj_slices[0].b * 1000,
                                trj_slices[0].e * 1000, trj_slices[0].dt)]))].copy()
@@ -676,23 +679,92 @@ def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice]) ->
         ignore_index=True).sort_values(
         ['system', 'CHL amount, %', 'timepoint', 'z'], ignore_index=True)
     merged['density'] = merged.density.astype(float)
-    merged = merged.join(merged.groupby(
-        ['system', 'CHL amount, %', 'timepoint', 'z'])['density'].sum(),
-        on=['system', 'CHL amount, %', 'timepoint', 'z'], rsuffix='_sum')
-    merged['density_fraction'] = merged['density'] / merged['density_sum']
     merged = merged.explode(['comp'])
     merged['comp'] = merged.comp.astype(str)
     merged = merged.join(pd.get_dummies(merged['comp']))
-    merged['comp_2_fr'] = merged['density_fraction'] * merged['2']
-    merged['comp_1_fr'] = merged['density_fraction'] * merged['1']
-    merged['comp_fr_sum'] = merged['comp_2_fr'] + merged['comp_1_fr']
-    merged['% of horizontal component'] = merged['comp_2_fr'] / \
-        merged['comp_fr_sum'] * 100
+    merged['comp_dens'] = merged['1'] * \
+        merged['density'] + merged['2'] * merged['density']
+    merged = merged.join(merged.groupby(
+        ['system', 'CHL amount, %', 'timepoint', 'z'])['comp_dens'].sum(),
+        on=['system', 'CHL amount, %', 'timepoint', 'z'], rsuffix='_sum')
+    merged['% of horizontal component'] = merged['2'] * \
+        merged['density'] / merged['comp_dens_sum'] * 100
     merged.to_csv(trj_slices[0].system.path / 'notebooks' /
-                  'integral_parameters' / 'tilt_density.csv')
+                  'integral_parameters' / 'tilt_density.csv', index=False)
 
 
-# %%
+def angl_dens_multiple_plot(experiments: dict,
+                            trj_slices: list[TrajectorySlice],
+                            df: pd.DataFrame, y: str, out: str) -> None:
+    '''
+    plot all density_angles plots
+    y -- name of variable in df which to plot
+    out -- tilt or horiz_comp in outfile name
+    '''
+
+    for exp, systs in experiments.items():
+        fig, axs = plt.subplots(3, 3, figsize=(
+            21, 21), sharex=True, sharey=True)
+        systs = [(s + '_chol10', s + '_chol30', s + '_chol50')
+                 for s in systs]
+        for s_row, ax_row in zip(systs, axs):
+            for s, ax in zip(s_row, ax_row):
+                ax2 = ax.twinx()
+                sns.histplot(data=df[(df['system'] == s.split('_chol', 1)[0])
+                                     & (df['CHL amount, %'] == int(s.split('_chol', 1)[1]))],
+                             x='z', y=y, ax=ax2, alpha=0.5, bins=50,
+                             stat='density')
+                try:
+                    plot_density_profile(ax,
+                                     TrajectorySlice(
+                                         System(trj_slices[0].system.path, s),
+                                         trj_slices[0].b, trj_slices[0].e, trj_slices[0].dt),
+                                     groups=['chols'], color='k')
+                    plot_density_profile(ax,
+                                     TrajectorySlice(
+                                         System(trj_slices[0].system.path, s),
+                                         trj_slices[0].b, trj_slices[0].e, trj_slices[0].dt),
+                                     groups=['phosphates'], color='C3')
+                except FileNotFoundError as e:
+                    print(e)
+
+                if ax != ax_row[-1]:
+                    ax2.set_ylabel('')
+                    ax2.yaxis.set_ticklabels([])
+                if ax != ax_row[0]:
+                    ax.set_ylabel('')
+                ax.set_xlabel('')
+                ax.set_title(
+                    f"{s.split('_chol', 1)[0]}, {s.split('_chol', 1)[1]} % CHL")
+        for ax in axs[-1, :]:
+            ax.set_xlabel('Z, nm')
+        labels, handles = axs[0, 0].get_legend_handles_labels()
+        fig.legend(labels, handles)
+        fig.suptitle(exp)
+        plt.savefig(trj_slices[0].system.path / 'notebooks' / 'integral_parameters' /
+                    f'{"_".join(exp.split())}_'
+                    f'density_{out}_{trj_slices[0].b}_{trj_slices[0].e}_{trj_slices[0].dt}.png',
+                    bbox_inches='tight')
+
+
+def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice]) -> None:
+    '''
+    calculate percentage of chl tilt horizontal component in systems
+    and plot it together with density plots;
+    9 plots per experiment
+    '''
+    trj_slices = [s for s in trj_slices if 'chol' in s.system.name]
+    if not (trj_slices[0].system.path / 'notebooks' /
+            'integral_parameters' / 'tilt_density.csv').is_file():
+        create_tilt_density_table(trj_slices)
+    df = pd.read_csv(trj_slices[0].system.path / 'notebooks' /
+                     'integral_parameters' / 'tilt_density.csv')
+    print('plotting horizontal component distribution of angles with CHL density profiles...')
+    angl_dens_multiple_plot(experiments, trj_slices, df,
+                            '% of horizontal component', 'hor_comp')
+    # print('plotting tilt distribution of angles with CHL density profiles...')
+    # angl_dens_multiple_plot(experiments, trj_slices, df, 'α, °', 'tilt')
+    print('done.')
 
 
 def parse_args():
@@ -740,6 +812,7 @@ def main():
     '''
     sns.set(style='ticks', context='talk', palette='bright')
     args = parse_args()
+    path = Path('/home/klim/Documents/chol_impact')
     experiments = {
         'chain length': ('dmpc', 'dppc_325', 'dspc'),
         'chain saturation': ('dppc_325', 'popc', 'dopc'),
