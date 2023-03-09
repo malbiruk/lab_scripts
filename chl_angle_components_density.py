e548bae23a4f71f0fb727fa3b1fb05b29d9369c4
malbiruk <2601074@gmail.com>
2023-03-09 10:51:53 +0300

    added mre flexibility to chl_tilt_angle function also added ability to have separate b e dt for components analysis and all other methods

--- chl_angle_components_density.py
+++ chl_angle_components_density.py
@@ -1,4 +1,22 @@
+'''
+Script to obtain chl angle components and draw CHL angle component+density plots.
+CHL ids and angle component are connected here.
+'''
+
+import argparse
+import sys
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from modules.general import flatten, opener, sparkles, duration
+from modules.traj import System, TrajectorySlice
+from modules.density import plot_density_profile
+from modules.constants import PATH, TO_RUS, EXPERIMENTS
 from integral_parameters_script import chl_tilt_angle
+
 
 def obtain_df_of_density(trj_slices: list[TrajectorySlice]) -> pd.DataFrame:
     '''
@@ -28,31 +46,28 @@ def obtain_df_of_density(trj_slices: list[TrajectorySlice]) -> pd.DataFrame:
     return df.explode(['chl_index', 'z', 'density'])
 
 
-def angle_refers_to_component(syst, angle, chl) -> tuple:
+def angle_refers_to_component(syst, angle, chl, b, e, dt) -> tuple:
     '''
     depending on system and chl amount determine component:
     1 vertical, 2 horizontal
     returns tuple (1,), (2,) or (1,2)
 
     ctr +- 1/2 wid => angle in component
-    components are located in notebooks/chol_tilt/{syst}_100-200-100_4_comps.csv
+    components are located in notebooks/chol_tilt/{syst}_{b}-{e}-{dt}_4_comps.csv
     '''
     comps = pd.read_csv('/home/klim/Documents/chol_impact/'
-                        f'notebooks/chol_tilt/{syst}_chol{chl}_100-200-100_4_comps.csv')
+                        f'notebooks/chol_tilt/{syst}_chol{chl}_{b}-{e}-{dt}_4_comps.csv')
+    comps = comps.sort_values(['ctr']).reset_index(drop=True)
     res = []
     if angle >= 0:
-        if (comps.loc[3, 'ctr'] - comps.loc[3, 'wid'] / 2
-                <= angle <= comps.loc[3, 'ctr'] + comps.loc[3, 'wid'] / 2):
+        if comps.loc[3, 'ctr'] - comps.loc[3, 'wid'] / 2 <= angle:
             res.append(2)
-        if (comps.loc[2, 'ctr'] - comps.loc[2, 'wid'] / 2
-                <= angle <= comps.loc[2, 'ctr'] + comps.loc[2, 'wid'] / 2):
+        if angle <= comps.loc[2, 'ctr'] + comps.loc[2, 'wid'] / 2:
             res.append(1)
     if angle < 0:
-        if (comps.loc[0, 'ctr'] + comps.loc[0, 'wid'] / 2
-                >= angle >= comps.loc[0, 'ctr'] - comps.loc[0, 'wid'] / 2):
+        if angle <= comps.loc[0, 'ctr'] + comps.loc[0, 'wid'] / 2:
             res.append(2)
-        if (comps.loc[1, 'ctr'] + comps.loc[1, 'wid'] / 2
-                >= angle >= comps.loc[1, 'ctr'] - comps.loc[1, 'wid'] / 2):
+        if angle >= comps.loc[1, 'ctr'] - comps.loc[1, 'wid'] / 2:
             res.append(1)
     return tuple(res)
 
@@ -63,7 +78,8 @@ def create_tilt_density_table(trj_slices: list[TrajectorySlice]) -> None:
     '''
 
     df = pd.read_csv(trj_slices[0].system.path / 'notebooks' /
-                     'integral_parameters' / 'chl_tilt_100-200-100.csv')
+                     'integral_parameters' /
+                     f'chl_tilt_{trj_slices[0].b}-{trj_slices[0].e}-{trj_slices[0].dt}.csv')
     df = df[df['timepoint'].astype(str).str.fullmatch('|'.join(
         [str(i) for i in range(trj_slices[0].b * 1000,
                                trj_slices[0].e * 1000, trj_slices[0].dt)]))].copy()
@@ -72,7 +88,8 @@ def create_tilt_density_table(trj_slices: list[TrajectorySlice]) -> None:
     print('determining angle components...')
     df['comp'] = df.apply(
         lambda row: angle_refers_to_component(
-            row['system'], row['α, °'], row['CHL amount, %']), axis=1)
+            row['system'], row['α, °'], row['CHL amount, %'],
+            trj_slices[0].b, trj_slices[0].e, trj_slices[0].dt), axis=1)
 
     print('reformatting density data...')
     dens = obtain_df_of_density(trj_slices).sort_values(
@@ -96,6 +113,8 @@ def create_tilt_density_table(trj_slices: list[TrajectorySlice]) -> None:
         ['system', 'CHL amount, %', 'timepoint', 'z'])['comp_dens'].sum(),
         on=['system', 'CHL amount, %', 'timepoint', 'z'], rsuffix='_sum')
     merged['% of horizontal component'] = merged['2'] * \
+        merged['density'] / merged['comp_dens_sum'] * 100
+    merged['% of vertical component'] = merged['1'] * \
         merged['density'] / merged['comp_dens_sum'] * 100
     merged.to_csv(trj_slices[0].system.path / 'notebooks' /
                   'integral_parameters' / 'tilt_density.csv', index=False)
@@ -157,7 +176,7 @@ def angl_dens_multiple_plot(experiments: dict,
                     bbox_inches='tight')
 
 
-def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice], to_rus: dict) -> None:
+def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice]) -> None:
     '''
     calculate percentage of chl tilt horizontal component in systems
     and plot it together with density plots;
@@ -165,7 +184,9 @@ def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice], to
     '''
     trj_slices = [s for s in trj_slices if 'chol' in s.system.name]
     if not (trj_slices[0].system.path / 'notebooks' /
-            'integral_parameters' / 'tilt_density.csv').is_file():
+            'integral_parameters' /
+            f'tilt_density_{trj_slices[0].b}-{trj_slices[0].e}-{trj_slices[0].dt}.csv'
+            ).is_file():
         create_tilt_density_table(trj_slices)
     df = pd.read_csv(trj_slices[0].system.path / 'notebooks' /
                      'integral_parameters' / 'tilt_density.csv')
@@ -174,3 +195,94 @@ def plot_angles_density(experiments: dict, trj_slices: list[TrajectorySlice], to
     angl_dens_multiple_plot(experiments, trj_slices, df,
                             '% of horizontal component', 'hor_comp')
     print('done.')
+
+
+def add_comps_to_chl_tilt(b: int, e: int, dt: int):
+    '''
+    add info which component is every angle in chl_tilt_{b}-{e}-{dt}.to_csv
+    (file produced by chl_tilt_angle() function)
+    '''
+    df = pd.read_csv(PATH / 'notebooks' /
+                     'integral_parameters' / f'chl_tilt_{b}-{e}-{dt}.csv')
+    print('determining angle components...')
+    df['comp'] = df.apply(
+        lambda row: angle_refers_to_component(
+            row['system'], row['α, °'], row['CHL amount, %'],
+            b, e, dt), axis=1)
+    df = df.explode(['comp'])
+    df['comp'] = df.comp.astype(str)
+    df = df.join(pd.get_dummies(df['comp']))
+    df.to_csv(PATH / 'notebooks' /
+              'integral_parameters' / f'chl_tilt_{b}-{e}-{dt}_with_comps.csv',
+              index=False)
+    print('done.')
+
+
+@ sparkles
+@ duration
+def main():
+    '''
+    parse arguments and execute all or some of the functions:
+    chl_tilt_angle, angle_components_density, angle_components_3d
+    '''
+    parser = argparse.ArgumentParser(
+        description='Script to obtain chl angle components and draw CHL angle '
+        'component+density plots')
+    parser.add_argument('--chl_tilt_angle',
+                        action='store_true',
+                        help='calculate angles between CHL c3-c17 vector and bilayer plane '
+                        'in each frame of trajectory for each CHL molecule, '
+                        'split to components')
+    parser.add_argument('--angle_components_density',
+                        action='store_true',
+                        help='plot percentage of component related to Z axis '
+                        'in combination with density plot')
+    parser.add_argument('--angle_components_3d',
+                        action='store_true',
+                        help='draw 3d plots of distribution of components')
+    parser.add_argument('--rus',
+                        action='store_true',
+                        help='plot graphs with russian captions.')
+    parser.add_argument('-b', '--b', type=int, default=150,
+                        help='beginning time in ns, default=150')
+    parser.add_argument('-e', '--e', type=int, default=200,
+                        help='ending time in ns, default=200')
+    parser.add_argument('-dt', '--dt', type=int, default=1000,
+                        help='dt in ps (default=1000)')
+    parser.add_argument('--chl_tilt_b_e_dt',
+                        nargs='+',
+                        default='100 200 100',
+                        help='b e dt (3 numbers) for calculation of tilt components, '
+                        'chl_tilt_angle will be calculated with this values. '
+                        '(100 200 100 by default)')
+
+    if len(sys.argv) < 2:
+        parser.print_usage()
+
+    args = parser.parse_args()
+
+    systems = flatten([(i, i + '_chol10', i + '_chol30', i + '_chol50')
+                       for i in flatten(EXPERIMENTS.values())])
+
+    trj_slices = [TrajectorySlice(
+        System(PATH, s), args.b, args.e, args.dt) for s in systems]
+
+    if args.chl_tilt_b_e_dt is not None:
+        chl_tilt_b, chl_tilt_e, chl_tilt_dt = [int(i)
+                                               for i in args.chl_tilt_b_e_dt.split()]
+
+        chl_tilt_trj_slices = [TrajectorySlice(
+            System(PATH, s), chl_tilt_b, chl_tilt_e, chl_tilt_dt) for s in systems]
+
+        chl_tilt_angle(chl_tilt_trj_slices, EXPERIMENTS, TO_RUS)
+        add_comps_to_chl_tilt(chl_tilt_b, chl_tilt_e, chl_tilt_dt)
+
+
+# %%
+if __name__ == '__main__':
+    main()
+
+# %%
+df = pd.read_csv(PATH / 'notebooks' /
+                 'integral_parameters' / f'chl_tilt_100-200-100_with_comps.csv')
+df
