680bc820f252d51ea74dfe36df955ba14a5a29c4
malbiruk <2601074@gmail.com>
2023-03-09 10:48:53 +0300

    added separate module with constants

---
+++ modules/constants.py
@@ -0,0 +1,22 @@
+'''
+this module contains often used constants
+'''
+from pathlib import Path
+
+PATH = Path('/home/klim/Documents/chol_impact')
+EXPERIMENTS = {
+    'chain length': ('dmpc', 'dppc_325', 'dspc'),
+    'chain saturation': ('dppc_325', 'popc', 'dopc'),
+    'head polarity': ('dopc', 'dopc_dops50', 'dops'),
+}
+TO_RUS = {'dmpc': 'ДМФХ', 'dppc_325': 'ДПФХ', 'dspc': 'ДСФХ', 'popc': 'ПОФХ',
+          'dopc': 'ДОФХ', 'dops': 'ДОФС', 'dopc_dops50': 'ДОФХ/ДОФС',
+          'chain length': 'Длина ацильных цепей',
+          'chain saturation': 'Насыщенность ацильных цепей',
+          'head polarity': 'Полярность "головок"',
+          'thickness, nm': 'Толщина, нм',
+          'distance, nm': 'Расстояние, нм',
+          'area per lipid, nm²': 'Средняяя площадь на липид, нм²',
+          'scd': 'Scd', 'peak width, nm': 'Ширина пиков профилей плотности ХС, нм',
+          'α, °': 'α, °'
+          }
