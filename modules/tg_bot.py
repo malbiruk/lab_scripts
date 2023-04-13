28eeafe8f58ae39fe5283b843e496d1465f1b9bf
malbiruk <2601074@gmail.com>
2023-04-13 15:07:45 +0300

    fixed obtain_trj_last_ns_dt1.py

--- modules/tg_bot.py
+++ modules/tg_bot.py
@@ -6,14 +6,13 @@ SEND_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 CHAT_ID = '258382605'
 
 
-
-# %%
 def send_message(message: str, silent: bool = False):
     '''
     send telegram message from lab_scripts bot.
     ** -- bold text, __ -- italics
     '''
-    message = '`' + traceback.extract_stack()[-2].filename.rsplit('/',1)[1] + '`: ' + message
+    message = ('`' + traceback.extract_stack()[-2]
+               .filename.rsplit('/', 1)[1] + '`: ' + message)
     answer = requests.post(SEND_URL,
                            json={'chat_id': CHAT_ID,
                                  'text': message,
